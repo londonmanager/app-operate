a4247b3e82b34b25fa29ca4d3d72c7ecfdcb44a7
arianf30 <arianfernandez@gmail.com>
2023-09-21 20:52:12 -0300

    fix: cors

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://londonmanager-login.onrender.com/'
+  base: 'https://londonmanager-login.onrender.com/',
+  cors: {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  }
 })
