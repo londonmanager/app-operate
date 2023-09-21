212ce52b17c441cf307e76276e01b0eb30014a8e
arianf30 <arianfernandez@gmail.com>
2023-09-21 20:37:42 -0300

    fix: baseUrl

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
-import { ssr } from 'vite-plugin-ssr/plugin'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ssr({
-      baseAssets: 'https://londonmanager-login.onrender.com/'
-    })
-  ]
+  plugins: [react()]
 })
