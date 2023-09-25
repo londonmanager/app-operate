913a892f5ed79e18386b8635e649074345b39df5
arianf30 <arianfernandez@gmail.com>
2023-09-25 20:07:21 -0300

    feat: add pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Auth from './layouts/Auth.jsx'
+import AuthLayout from './layouts/AuthLayout.jsx'
+import Login from './pages/Login.jsx'
+import SignUp from './pages/SignUp.jsx'
 import './index.css'
+import ResetPassword from './pages/ResetPassword.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: (
+      <Auth>
+        <AuthLayout>
+          <Login />
+        </AuthLayout>
+      </Auth>
+    )
+  },
+  {
+    path: '/signup',
+    element: (
+      <Auth>
+        <AuthLayout>
+          <SignUp />
+        </AuthLayout>
+      </Auth>
+    )
+  },
+  {
+    path: '/reset-password',
+    element: (
+      <Auth>
+        <AuthLayout>
+          <ResetPassword />
+        </AuthLayout>
+      </Auth>
+    )
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
