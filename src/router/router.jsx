1eb5024f65891bbeb8175a3afec53ad6cabbb39f
arianf30 <arianfernandez@gmail.com>
2024-03-17 17:51:17 -0300

    router fix

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom'
-import { Auth } from '../layouts/Auth.jsx'
+// import { Auth } from '../layouts/Auth.jsx'
 import Sale from '../pages/Sale.jsx'
 
 const router = createBrowserRouter([
