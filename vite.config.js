b5d070b98f06e0740fddea2d0c8f34c783459316
arianf30 <arianfernandez@gmail.com>
2024-03-17 22:18:23 -0300

    build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const BASE_URL = 'https://d1q3n7jfsudqp9.cloudfront.net/'
+const BASE_URL = 'https://d1q3n7jfsudqp9.cloudfront.net'
 const BASE_URL_LOCAL = 'http://localhost:5173'
 
 export default defineConfig({
