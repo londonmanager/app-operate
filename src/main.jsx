45ee361ff0f65c0f0c3b6140539e453e91bbaf81
arianf30 <arianfernandez@gmail.com>
2024-03-18 02:34:15 -0300

    build new

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import router from './router/router'
 import './index.scss'
 
-const queryClient = new QueryClient()
+// const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    {/* <QueryClientProvider client={queryClient}> */}
       <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={true} />
-    </QueryClientProvider>
+      {/* <ReactQueryDevtools initialIsOpen={true} /> */}
+    {/* </QueryClientProvider> */}
   </React.StrictMode>
 )
